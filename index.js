753509bb05bdceb1602f310c557539be2e943a94
Gage Henderson <gage@quangoinc.com>
2018-08-24 11:07:03 -0700

    Obama hated es6

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function getRandom(amount) {
   return Math.floor(Math.random() * (amount + 1));
 }
 
-exports.default = function() {
+export default function() {
   return tweets[getRandom(0, tweets.length)];
-};
+}
